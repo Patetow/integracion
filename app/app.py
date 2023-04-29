0bac053f8041d9185a4275bea639113c36b554e1
Patetow <pa.toros@duocuc.cl>
2023-04-29 17:19:06 -0400

    xxxx

--- app/app.py
+++ app/app.py
@@ -4,6 +4,16 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+@app.route('/products')
+def mostrar_productos():
+    # Abrir el archivo JSON
+    with open('productos.json') as archivo:
+        # Cargar los datos del archivo en un objeto Python
+        datos = json.load(archivo)
+    # Obtener la lista de productos del objeto JSON
+    productos = datos['productos']
+    # Pasar la lista de productos a la plantilla Jinja2
+    return render_template('products.html', productos=productos)
 
 
 
